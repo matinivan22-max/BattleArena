c2a9dcf5950db8a05e5f6e589385aea7899ef7c9
matinivan22-max <matinivan22@gmail.com>
2026-09-07 10:56:29 +0800

    First Warrior

---
+++ BattleArena/BattleArena/Program.cs
@@ -0,0 +1,46 @@
+﻿using BattleArena.Warriors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior Etits = new Warrior();
+            Etits.Name = "Bading";
+            Etits.Health = 300;
+            Etits.AttackPower = 50;
+
+            Warrior Orbeast = new Warrior();
+            Orbeast.Name = "Orbeast";
+            Orbeast.Health = 10;
+            Orbeast.AttackPower = 1;
+
+            Warrior Dedong = new Warrior();
+            Dedong.Name = "Dedong";
+            Dedong.Health = 100;
+            Dedong.AttackPower = 20;
+
+
+            Console.WriteLine($"{Etits.Name} has " +
+                $"{Etits.Health} health and {Etits.AttackPower} attack power.");
+            Console.WriteLine("----------------------------------------------");
+
+            Console.WriteLine($"{Orbeast.Name} has " +
+                $"{Orbeast.Health} health and {Orbeast.AttackPower} attack power.");
+            Console.WriteLine("----------------------------------------------");
+
+            Console.WriteLine($"{Dedong.Name} has " +
+                $"{Dedong.Health} health and {Dedong.AttackPower} attack power.");
+            Console.WriteLine("----------------------------------------------");
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ BattleArena/BattleArena/Warriors/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena.Warriors
+{
+    internal class Warrior
+    {
+        public string Name;
+        public int Health;
+        public int AttackPower;
+
+        public Warrior()
+        {
+            
+        }
+    }
+}
