2063934ab2f2a228c39f2c1c695ab5c448bf8ebe
matinivan22-max <matinivan22@gmail.com>
2026-09-07 11:25:25 +0800

    Intro to Constructor and Properties

--- BattleArena/BattleArena/Program.cs
+++ BattleArena/BattleArena/Program.cs
@@ -1,46 +1,34 @@
-﻿using BattleArena.Warriors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using BattleArena.Warriors;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace BattleArena
-{
-    internal partial class Program
+    namespace BattleArena
     {
-        static void Main(string[] args)
+        internal partial class Program
         {
-            Warrior Etits = new Warrior();
-            Etits.Name = "Bading";
-            Etits.Health = 300;
-            Etits.AttackPower = 50;
+            static void Main(string[] args)
+            {
+                Warrior Etits = new Warrior("Etits", 300, 50);
+                Warrior Orbeast = new Warrior("Orbeast", 10, 1);
+                Warrior Dedong = new Warrior("Dedong", 100, 20);
 
-            Warrior Orbeast = new Warrior();
-            Orbeast.Name = "Orbeast";
-            Orbeast.Health = 10;
-            Orbeast.AttackPower = 1;
+                Console.WriteLine($"{Etits.Name} has " +
+                    $"{Etits.Health} health and {Etits.AttackPower} attack power.");
+                Console.WriteLine("----------------------------------------------");
 
-            Warrior Dedong = new Warrior();
-            Dedong.Name = "Dedong";
-            Dedong.Health = 100;
-            Dedong.AttackPower = 20;
+                Console.WriteLine($"{Orbeast.Name} has " +
+                    $"{Orbeast.Health} health and {Orbeast.AttackPower} attack power.");
+                Console.WriteLine("----------------------------------------------");
 
+                Console.WriteLine($"{Dedong.Name} has " +
+                    $"{Dedong.Health} health and {Dedong.AttackPower} attack power.");
+                Console.WriteLine("----------------------------------------------");
 
-            Console.WriteLine($"{Etits.Name} has " +
-                $"{Etits.Health} health and {Etits.AttackPower} attack power.");
-            Console.WriteLine("----------------------------------------------");
+                Console.ReadLine();
+            }
 
-            Console.WriteLine($"{Orbeast.Name} has " +
-                $"{Orbeast.Health} health and {Orbeast.AttackPower} attack power.");
-            Console.WriteLine("----------------------------------------------");
-
-            Console.WriteLine($"{Dedong.Name} has " +
-                $"{Dedong.Health} health and {Dedong.AttackPower} attack power.");
-            Console.WriteLine("----------------------------------------------");
-
-            Console.ReadLine();
         }
-
     }
-}
--- BattleArena/BattleArena/Warriors/Warrior.cs
+++ BattleArena/BattleArena/Warriors/Warrior.cs
@@ -8,13 +8,15 @@ namespace BattleArena.Warriors
 {
     internal class Warrior
     {
-        public string Name;
-        public int Health;
-        public int AttackPower;
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int AttackPower { get; private set; }
 
-        public Warrior()
+        public Warrior(string name, int health, int attackPower)
         {
-            
+            Name = name;
+            Health = health;
+            AttackPower = attackPower;
         }
     }
 }
